12e475a6d9ec9c3d133f8e66410a678663e4d302
leah wanjiku <leagm22@gmail.com>
2023-03-13 09:35:20 +0300

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,11 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students =Student.all
     render json: students
   end
-
+   def show
+    students=Student.find(params[:id])
+    render json: students
+   end
 end
